c7c1d050ecba63df8a5f1b94c9ab336edc62863e
arturososasalazar3-svg <arturososasalazar3@gmail.com>
2025-10-23 03:05:38 -0600

    Add files via upload

--- handler.js
+++ handler.js
@@ -24,10 +24,10 @@ export async function handler(chatUpdate) {
     let m = chatUpdate.messages[chatUpdate.messages.length - 1];
     if (!m) return;
 
-    m = smsg(this, m) || m;
+   /* m = smsg(this, m) || m;
     if (!m) return;
 
-    if (!m.isGroup) return;
+    if (!m.isGroup) return;*/
 
     this.processedMessages = this.processedMessages || new Map();
     const now = Date.now();
